9c4ee75bdc3f95cf933d41daa004977576e6b2da
GeraltOfRiviaa <samuelsvob@gmail.com>
2026-04-29 13:59:32 +0200

    Updating views and urls to display course titles using their indexes

--- e-learning-django/courses/urls.py
+++ e-learning-django/courses/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("<int:id>/", views.course_id, name="overview"),
 ]
--- e-learning-django/courses/views.py
+++ e-learning-django/courses/views.py
@@ -1,2 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Course
+
 # Create your views here.
+def course_id(request, id):
+    context = {
+        "course" : Course.objects.get(pk=id)
+    }
+    return HttpResponse(context["course"])
+
